5e2d2bfeb3c06e629bbf1cf9a6bc2d0c3673fd92
Katelyn Rohrer <k.rohrer03@gmail.com>
2023-10-28 15:34:57 -0700

    working background

--- src/view_controller/gameScreen.java
+++ src/view_controller/gameScreen.java
@@ -1,5 +1,52 @@
 package view_controller;
 
-public class gameScreen {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class gameScreen extends Application {
+    BorderPane root;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        root = new BorderPane();
+        System.out.println("space invaders!!");
+        Scene scene = new Scene(root, 500, 700);
+        setBackground();
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void setBackground() {
+        try {
+            InputStream inStream = new FileInputStream("lib/game-background.jpg");
+            Image image = new Image(inStream);
+            BackgroundImage bgImage = new BackgroundImage(image,
+                    BackgroundRepeat.NO_REPEAT,
+                    BackgroundRepeat.NO_REPEAT,
+                    BackgroundPosition.DEFAULT,
+                    BackgroundSize.DEFAULT);
+            Background bg = new Background(bgImage);
+            root.setBackground(bg);
+        } catch (FileNotFoundException e) {
+            System.out.println("Background image not found. Setting to default black.");
+            Paint c = Color.BLACK;
+            Background bg = new Background(new BackgroundFill(c, null, null));
+            root.setBackground(bg);
+        }
+    }
 
 }
