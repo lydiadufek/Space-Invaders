c8c6f09b894c67669ddfe0121a6ceb0bc137184b
Katelyn Rohrer <k.rohrer03@gmail.com>
2023-10-30 17:07:36 -0700

    got the top bar display
    assumes no max ship count, which may cause problems later with the shifting size of the lives section. might go back later

--- src/view_controller/gameScreen.java
+++ src/view_controller/gameScreen.java
@@ -1,19 +1,27 @@
 package view_controller;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 public class gameScreen extends Application {
     BorderPane root;
+    Label scoreNum;
+    BorderPane topBar;
+    VBox livesBox;
 
     public static void main(String[] args) {
         launch(args);
@@ -24,7 +32,7 @@ public class gameScreen extends Application {
         root = new BorderPane();
         System.out.println("space invaders!!");
         Scene scene = new Scene(root, 500, 700);
-        setBackground();
+        setupGUI();
 
         stage.setScene(scene);
         stage.show();
@@ -35,8 +43,8 @@ public class gameScreen extends Application {
             InputStream inStream = new FileInputStream("lib/game-background.jpg");
             Image image = new Image(inStream);
             BackgroundImage bgImage = new BackgroundImage(image,
-                    BackgroundRepeat.NO_REPEAT,
-                    BackgroundRepeat.NO_REPEAT,
+                    BackgroundRepeat.REPEAT,
+                    BackgroundRepeat.REPEAT,
                     BackgroundPosition.DEFAULT,
                     BackgroundSize.DEFAULT);
             Background bg = new Background(bgImage);
@@ -49,4 +57,89 @@ public class gameScreen extends Application {
         }
     }
 
+    private void setupGUI() {
+        setBackground();
+        setupTopBar();
+    }
+
+    private Font getFont() {
+        FileInputStream fontInputStream;
+        try {
+            fontInputStream = new FileInputStream("lib/pixeboy-font.ttf");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return Font.loadFont(fontInputStream, 25);
+    }
+
+    private void setupTopBar() {
+        topBar = new BorderPane();
+        HBox scoreBox = new HBox();
+        scoreBox.setSpacing(10);
+
+        livesBox = new VBox();
+        livesBox.setSpacing(5);
+
+        Font font = getFont();
+
+        // initialize score label
+        Label scoreLabel = new Label("  SCORE");
+        scoreLabel.setFont(font);
+        scoreLabel.setTextFill(Color.WHITE);
+
+        // initialize score number label
+        scoreNum = new Label("0");
+        scoreNum.setFont(font);
+        scoreNum.setTextFill(Color.LIMEGREEN);
+
+        scoreBox.getChildren().addAll(scoreLabel, scoreNum);
+        topBar.setLeft(scoreBox);
+        topBar.setPadding(new Insets(10, 10, 10, 10));
+
+        setLivesDisplay(10);
+
+        root.setTop(topBar);
+    }
+
+    private void setLivesDisplay(int num) {
+        ArrayList<HBox> paneList = new ArrayList<>();
+        HBox firstRow = new HBox();
+        firstRow.setSpacing(5);
+        paneList.add(firstRow);
+        ArrayList<ImageView> extraShips = new ArrayList<>();
+
+        Font font = getFont();
+
+        Label livesLabel = new Label("LIVES ");
+        livesLabel.setFont(font);
+        livesLabel.setTextFill(Color.WHITE);
+        livesBox.getChildren().add(paneList.get(0));
+        paneList.get(0).getChildren().add(livesLabel);
+
+        FileInputStream shipImagePath;
+        try {
+            shipImagePath = new FileInputStream("lib/ship.png");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        Image shipImageObj = new Image(shipImagePath, 40, 40, true, false);
+        for (int i = 0; i < num; i++) {
+            extraShips.add(new ImageView(shipImageObj));
+        }
+
+        for (int i = 0; i < extraShips.size(); i++) {
+            if (i != 0 && i % 3 == 0) {
+                HBox temp = new HBox();
+                paneList.add(temp);
+                temp.setSpacing(5);
+                temp.getChildren().add(new Label("\t\t\t"));
+                livesBox.getChildren().add(temp);
+
+            }
+            paneList.get(i/3).getChildren().add(extraShips.get(i));
+        }
+
+        topBar.setRight(livesBox);
+    }
 }
