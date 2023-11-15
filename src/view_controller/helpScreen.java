ab79fe3d9825264a47b76233a5925f2d2d0a121b
Katelyn Rohrer <k.rohrer03@gmail.com>
2023-11-15 14:12:59 -0700

    cleaning up helpScreen, removed start from helpScreen

--- src/view_controller/helpScreen.java
+++ src/view_controller/helpScreen.java
@@ -1,62 +1,32 @@
 package view_controller;
 
-import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
-import javafx.stage.Stage;
 import model.Utils;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.util.Optional;
-
-public class helpScreen extends Application {
+public class helpScreen {
     private BorderPane root;
     private Scene scene;
     private startScreen home;
     private Hyperlink backButton;
 
-    private final int WW = startScreen.getWW();
-    private final int WH = startScreen.getWH();
+    private static final int WW = startScreen.getWW();
+    private static final int WH = startScreen.getWH();
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage stage) {
-        root = new BorderPane();
-        scene = new Scene(root, WW, WH);
-
-        setBackground();
-        setupGUI();
-        registerHandlers();
-
-        stage.setScene(scene);
-        stage.show();
-    }
     public helpScreen(startScreen home) {
         this.home = home;
         root = new BorderPane();
-//        System.out.println("space invaders!!");
         scene = new Scene(root, WW, WH);
 
         setBackground();
         setupGUI();
         registerHandlers();
-
     }
 
     public Scene getScene() {
@@ -94,7 +64,5 @@ public class helpScreen extends Application {
             home.getStage().setScene(home.getScene());
             home.getStage().show();
         });
-
-
     }
 }
